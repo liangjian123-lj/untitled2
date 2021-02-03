7517ebb474d82d2243b0ab2405fba387a272187d
Liangjian <1346989102@qq.com>
2021-02-03 10:46:54 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el:'#app',
+    data:{
+        books:[
+            {
+                id:1,
+                name:'《算法导论》',
+                date:'2006-09',
+                price:85.00,
+                count:1
+            },
+            {
+                id:2,
+                name:'《UNIX》',
+                date:'2006-09',
+                price:26.00,
+                count:1
+            },
+            {
+                id:3,
+                name:'《编程珠玑》',
+                date:'2006-09',
+                price:29.00,
+                count:1
+            },
+            {
+                id:4,
+                name:'《计算机导论》',
+                date:'2006-09',
+                price:55.00,
+                count:1
+            },
+        ]
+    }
+})
